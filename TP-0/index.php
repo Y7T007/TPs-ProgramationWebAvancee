f9fc354bf78aca283f547b03e7674671bc222363
Y7T007 <yassir7t@gmail.com>
2024-02-01 17:09:39 +0100

    Add email numbering to the output

--- TP-0/index.php
+++ TP-0/index.php
@@ -3,9 +3,11 @@ $emails = array(file_get_contents('Emails.txt'));
 
 
 echo "hello world";
+$n=0;
 if (isset($emails)){
     foreach($emails as $email){
-        print $email."<br>";
+        print "Email $n : ".$email."<br>";
+        $n+=1;
     }
 }
 
