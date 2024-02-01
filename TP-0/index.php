04a0fb92b53a1b3ce4f4e449217974920edef6c9
Y7T007 <yassir7t@gmail.com>
2024-02-01 17:05:58 +0100

    Refactor email retrieval and display logic

--- TP-0/index.php
+++ TP-0/index.php
@@ -1,7 +1,11 @@
 <?php
-$emails = file_get_contents('Emails.txt');
+$emails = array(file_get_contents('Emails.txt'));
 
-echo $emails;
+if (isset($emails)){
+    foreach($emails as $email){
+        echo $email;
+    }
+}
 
 // if (isset($emails)){
 //     foreach($emails as $email){
