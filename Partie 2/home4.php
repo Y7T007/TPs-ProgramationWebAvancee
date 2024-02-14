e32da06ee179ad76b1eddf22e452ff492c7f6c1a
Y7T007 <yassir7t@gmail.com>
2024-02-14 02:06:22 +0100

    TP-2 DONE PREPARING FOR DEPLOYEMENT

--- Partie 2/home4.php
+++ Partie 2/home4.php
@@ -296,6 +296,20 @@ session_start();?>
 			</div>
 		</div>
 	</div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <button id="downloadPdf">Download PDF</button>
+
+        <script>
+            // JavaScript code for PDF generation
+            document.getElementById('downloadPdf').addEventListener('click', function () {
+                // Target the HTML element to be converted to PDF
+                var element = document.getElementById('yourRootElementId'); // Change 'yourRootElementId' to the actual ID of the root element
+
+                // Generate PDF
+                html2pdf(element);
+            });
+        </script>
 	<!-- Footer section start -->
 
 	<!-- Footer section end -->
