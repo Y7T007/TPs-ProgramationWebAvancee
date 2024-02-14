a70d51346016a73d2d9009c110b59487d61605b2
Y7T007 <yassir7t@gmail.com>
2024-02-14 01:57:16 +0100

    TP-2 DONE PREPARING FOR DEPLOYEMENT

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    <!-- Your content goes here -->
+
+    <div class="container center">
+        <a class="waves-effect waves-light btn-large" href="#partie1">Partie 1</a>
+        <a class="waves-effect waves-light btn-large" href="./Partie%202/index.php">Partie 2 </a>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
